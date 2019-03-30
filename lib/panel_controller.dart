34dcf1cb7127aab377a8c2cc38e15d220354b963
Akshath Jain <akshath.r.jain@gmail.com>
2019-03-30 18:03:06 -0400

    added a basic controller class

---
+++ lib/panel_controller.dart
@@ -0,0 +1,43 @@
+/*
+Name: Akshath Jain
+Date: 3/30/19
+Purpose: defines the panel controller class
+Copyright: © 2019, Akshath Jain. All rights reserved.
+Licensing: More information can be found here: https://github.com/akshathjain/sliding_up_panel/blob/master/LICENSE
+*/
+
+import 'package:flutter/material.dart';
+
+class PanelController{
+  VoidCallback _closeListener;
+  VoidCallback _openListener;
+  VoidCallback _hideListener;
+
+  @protected
+  void addCloseListener(VoidCallback listener){
+    this._closeListener = listener;
+  }
+
+  void close(){
+    _closeListener();
+  }
+
+  @protected
+  void addOpenListener(VoidCallback listener){
+    this._closeListener = listener;
+  }
+
+  void open(){
+    _openListener();
+  }
+
+  @protected
+  void addHideListener(VoidCallback listener){
+    this._closeListener = listener;
+  }
+
+  void hide(){
+    _hideListener();
+  }
+
+}
